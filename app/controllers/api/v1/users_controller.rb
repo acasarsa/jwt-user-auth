68522cf177730cde55148fd5c7b53456e89714e2
github username <github email address>
2020-08-28 13:12:31 -0400

    stuck on skip_before_action error

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,16 @@
 class Api::V1::UsersController < ApplicationController
     before_action :find_user, only: [:update, :destroy]
-    
+    # byebug
+    skip_before_action :authorized
 
-    def index
+    def profile
         render json: User.all
     end
 
     def create
         user = User.create(user_params)
         if user.valid? 
+            token = encode_token(user_id: user.id)
             render json: { user: UserSerializer.new(user) }, status: :created
         else 
             render json: { error: 'failed to create user' }, status: :not_acceptable
@@ -16,9 +18,11 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def update
+        # add routes
         user.update(user_params)
 
         if user.valid?
+            token = encode_token(user_id: user.id)
             render json: { user: UserSerializer.update(user) }, status: :updated
         else 
             render json: { error: 'failed to update user' }, status: :not_acceptable
@@ -26,6 +30,8 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def destroy
+        # add routes
+        token = encode_token(user_id: user.id)
         user.destroy
     end
 
@@ -36,7 +42,7 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def user_params
-        params.require(:user).permit(:username, :password_digest, :bio, :avatar )
+        params.require(:user).permit(:username, :password, :bio, :avatar )
     end
 
 end
